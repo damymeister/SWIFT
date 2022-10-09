cd2c6e18f4c5e93dbbc28bca0331acd1cb4b04e2
damymeister <77884237+damymeister@users.noreply.github.com>
2022-10-09 21:39:46 +0200

    Create L1-Z1.swift

---
+++ L1-Z1.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  L1_Z1
+//
+//  Created by student on 03/10/2022.
+//
+
+import Foundation
+var s = 0
+var avg = 0.0
+var a = Int.random(in:0...100)
+s += a
+a = Int.random(in:0...100)
+s += a
+a = Int.random(in:0...100)
+s += a
+avg = Double(s)/3.0
+print("Suma:",s,"Srednia:",avg)
