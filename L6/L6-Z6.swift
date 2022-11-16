dcd5e7324e6eba32e80042e7016499adfdcdb736
damymeister <77884237+damymeister@users.noreply.github.com>
2022-11-16 21:35:49 +0100

    Update L6-Z6.swift

--- L6/L6-Z6.swift
+++ L6/L6-Z6.swift
@@ -39,12 +39,30 @@ for i in 0 ..< wiersze {
     print("]", terminator: ", ")
 }
 print("]")
-
+var max_liczba : Float = tab2D[0][0]
+var max_index_i : Int = 0
+var max_index_j : Int = 0
+var min_liczba : Float = tab2D[0][0]
+var min_index_i : Int = 0
+var min_index_j : Int = 0
 for i in 0 ..< wiersze
 {
-    for j in 0..< kolumny
+    for j in 0 ..< kolumny
     {
-        if(
+        if(tab2D[i][j] > max_liczba)
+        {
+            max_liczba = tab2D[i][j]
+            max_index_i = i
+            max_index_j = j
+        }
+        if(tab2D[i][j] < min_liczba)
+        {
+            min_liczba = tab2D[i][j]
+            min_index_i = i
+            min_index_j = j
+        }
     }
 }
-//dokonczyc
+
+print("Element maksymalny to \(max_liczba) i znajduje sie w wierszu \(max_index_i) i kolumnie \(max_index_j)")
+print("Element minimalny to \(min_liczba) i znajduje sie w wierszu \(min_index_i) i kolumnie \(min_index_j)")
