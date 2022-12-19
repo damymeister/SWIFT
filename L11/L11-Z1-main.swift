c2b7c4d559d4bc1f86a7ad3460d3c9f39b4f773d
damymeister <77884237+damymeister@users.noreply.github.com>
2022-12-19 18:35:13 +0100

    Create L11-Z1-main.swift

---
+++ L11/L11-Z1-main.swift
@@ -0,0 +1,10 @@
+var prostokat1 = Prostokat(a: 5.5, b: 4.0)
+var prostokat2 = Prostokat(a: 6.0, b: 15.0)
+prostokat1.wyswietl()
+print("Pole prostokata jest rowne \(prostokat1.pole())")
+print("Obwod prostokata jest rowny \(prostokat1.obw())")
+prostokat2.wyswietl()
+print("Pole prostokata jest rowne \(prostokat2.pole())")
+print("Obwod prostokata jest rowny \(prostokat2.obw())")
+
+
