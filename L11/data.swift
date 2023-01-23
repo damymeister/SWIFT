3c08bc57fd4aac53294bba1a0646b33b53d86254
damymeister <77884237+damymeister@users.noreply.github.com>
2023-01-23 17:07:38 +0100

    Update data.swift

--- L11/data.swift
+++ L11/data.swift
@@ -36,10 +36,12 @@ print(z)
 
 
 
+import Foundation
 
- import Foundation
- 
- enum obywatelstwo: String, CaseIterable{
+
+
+
+enum obywatelstwo: String, CaseIterable{
     case hiszpan = "Hiszpanskie"
     case niemiec = "Niemieckie"
     case polak = "Polskie"
@@ -50,9 +52,9 @@ print(z)
     var Nazwisko : String = "" 
     var Waga : Double = 0.0 
     var Wzrost : Double = 0.0
-    var Pesel : Int = 0
+    var Pesel : String = ""
     var Kraj : obywatelstwo?
-    init(Imie: String, Nazwisko: String, Waga: Double, Wzrost: Double, Pesel: Int, Kraj: obywatelstwo?){
+    init(Imie: String, Nazwisko: String, Waga: Double, Wzrost: Double, Pesel: String, Kraj: obywatelstwo?){
         self.Imie = Imie
         self.Nazwisko = Nazwisko
         self.Waga = Waga
@@ -60,14 +62,46 @@ print(z)
         self.Pesel = Pesel
         self.Kraj = Kraj
     }
-    func show() -> Void{
-        print("Imie: \(Imie) - Nazwisko: \(Nazwisko) - Waga: \(Waga) - Wzrost: \(Wzrost) - obywatelstwo: \(Kraj!) ")
+    
+    func calcWiek () {
+                
+        
+        var tab = Array(Pesel)
+        var rok_index = String(tab[1])
+        print("tab:", tab)
+        var rok : String = "20" + String(tab[0]) + String(tab[1])
+        var mies : String = ""
+        if(tab[2] == "2")
+        {
+            mies += "0"
+        }
+        else{
+            mies += "1"
+        }
+        mies += String(tab[3])
+        var dzien : String = String(tab[4]) + String(tab[5])
+        var data : String = rok + "/" + mies + "/" + dzien
+        print(data)
+        
+        
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd"
+        let date = formatter.date(from: data)
+        let date2 = formatter.date(from: "2022/12/20")
+        var i = date!.distance(to:date2!)
+        var z = i / 31536000
+        print("wiek wynosi:", z)
+        
     }
+            func show() -> Void{
+                print("Imie: \(Imie) - Nazwisko: \(Nazwisko) - Waga: \(Waga) - Wzrost: \(Wzrost) - obywatelstwo: \(Kraj!) ")
+            }
  }
 
 
-var ob1 = Osoba(Imie: "Tomek", Nazwisko: "Domek", Waga: 43.2, Wzrost: 182, Pesel: 12312345678, Kraj: obywatelstwo.polak)
-
+var ob1 = Osoba(Imie: "Tomek", Nazwisko: "Domek", Waga: 43.2, Wzrost: 182, Pesel: "01221206677", Kraj: obywatelstwo.polak)
+ob1.calcWiek()
 
  enum rodzaj: String, CaseIterable{
     case signle = "signle"
@@ -82,7 +116,7 @@ class Tenisista : Osoba{
     var sumaPkt : Int = 0
     var NajlepszaPozycja : Int = 0
     var historiaMeczy : [(NazwaTurnieju: String, LiczbaPunktow: Int)] = []  
-    init(rodzajGry: rodzaj?, aktPozycja: Int, sumaPkt: Int, NajlepszaPozycja: Int, historiaMeczy: [(NazwaTurnieju: String, LiczbaPunktow: Int)],Imie: String, Nazwisko: String, Waga: Double, Wzrost: Double, Pesel: Int, Kraj: obywatelstwo?){
+    init(rodzajGry: rodzaj?, aktPozycja: Int, sumaPkt: Int, NajlepszaPozycja: Int, historiaMeczy: [(NazwaTurnieju: String, LiczbaPunktow: Int)],Imie: String, Nazwisko: String, Waga: Double, Wzrost: Double, Pesel: String, Kraj: obywatelstwo?){
         self.rodzajGry = rodzajGry
         self.aktPozycja = aktPozycja
         self.sumaPkt = sumaPkt
@@ -115,14 +149,13 @@ class Tenisista : Osoba{
     
 }
 
-var Tenis1 = Tenisista(rodzajGry: rodzaj.singleAndDouble, aktPozycja: 71, sumaPkt: 256, NajlepszaPozycja: 69, historiaMeczy: [(NazwaTurnieju: "Cos", LiczbaPunktow: 21)],Imie: "Tomek", Nazwisko: "Domek", Waga: 43.2, Wzrost: 182, Pesel: 12312345678, Kraj: obywatelstwo.polak)
-Tenis1.show()
-Tenis1.addTour()
+var Tenis1 = Tenisista(rodzajGry: rodzaj.singleAndDouble, aktPozycja: 71, sumaPkt: 256, NajlepszaPozycja: 69, historiaMeczy: [(NazwaTurnieju: "Cos", LiczbaPunktow: 21)],Imie: "Tomek", Nazwisko: "Domek", Waga: 43.2, Wzrost: 182, Pesel: "12312345678", Kraj: obywatelstwo.polak)
 Tenis1.show()
 
+
 var Tenis2 : [Tenisista] = []
-Tenis2.append(Tenisista(rodzajGry: rodzaj.double, aktPozycja: 71, sumaPkt: 256, NajlepszaPozycja: 69, historiaMeczy: [(NazwaTurnieju: "Cos", LiczbaPunktow: 21)],Imie: "Tomek", Nazwisko: "Domek", Waga: 43.2, Wzrost: 182, Pesel: 12312345678, Kraj: obywatelstwo.polak))
-Tenis2.append(Tenisista(rodzajGry: rodzaj.double, aktPozycja: 71, sumaPkt: 256, NajlepszaPozycja: 69, historiaMeczy: [(NazwaTurnieju: "Cos", LiczbaPunktow: 21)],Imie: "Maciek", Nazwisko: "Domek", Waga: 43.2, Wzrost: 182, Pesel: 12312345678, Kraj: obywatelstwo.hiszpan))
+Tenis2.append(Tenisista(rodzajGry: rodzaj.double, aktPozycja: 71, sumaPkt: 256, NajlepszaPozycja: 69, historiaMeczy: [(NazwaTurnieju: "Cos", LiczbaPunktow: 21)],Imie: "Tomek", Nazwisko: "Domek", Waga: 43.2, Wzrost: 182, Pesel: "12312345678", Kraj: obywatelstwo.polak))
+Tenis2.append(Tenisista(rodzajGry: rodzaj.double, aktPozycja: 71, sumaPkt: 256, NajlepszaPozycja: 69, historiaMeczy: [(NazwaTurnieju: "Cos", LiczbaPunktow: 21)],Imie: "Maciek", Nazwisko: "Domek", Waga: 43.2, Wzrost: 182, Pesel: "12312345678", Kraj: obywatelstwo.hiszpan))
 func printiszpan (tab: [Tenisista]) -> Void {
 
     for i in 0..<tab.count {
@@ -135,3 +168,4 @@ func printiszpan (tab: [Tenisista]) -> Void {
 }
 
 printiszpan(tab: Tenis2)
+Tenis1.calcWiek()
