e46df72d325c06761fe447bd67eaf5c82dcac71f
damymeister <77884237+damymeister@users.noreply.github.com>
2023-01-23 12:09:46 +0100

    Update data.swift

--- L11/data.swift
+++ L11/data.swift
@@ -1,9 +1,15 @@
 import Foundation
 
+
+var pesel : String = "01221206677"
+var peselchar = Array(pesel)
+print(peselchar)
+
+
 let formatter = DateFormatter()
 formatter.dateFormat = "yyyy/MM/dd"
 let date = formatter.date(from: "2021/12/20")
 let date2 = formatter.date(from: "2022/12/20")
 var i = date!.distance(to:date2!)
 var z = i / 31536000
-print(z) / 1.0
+print(z)
